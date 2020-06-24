be96187e59b0437f69d63926625cf5bdda76ebc2
tr1ckz <48994713+tr1ckz@users.noreply.github.com>
2020-06-24 00:33:38 -0400

    add cardealer and mechanic

--- utk_shiftlog/client.lua
+++ utk_shiftlog/client.lua
@@ -7,19 +7,19 @@ AddEventHandler("esx:playerLoaded", function(xPlayer)
     local job = xPlayer.job.name
 
     currentjob = job
-    if job == "police" or job == "ambulance" then -- job's name here
+    if job == "police" or job == "ambulance" or job == "mechanic" or job == "cardealer" then -- job's name here
         TriggerServerEvent("utk_sl:userjoined", job)
     end
 end)
 
 RegisterNetEvent('esx:setJob')
 AddEventHandler('esx:setJob', function(job)
-    if currentjob == "police" or currentjob == "ambulance" then -- job's name here
+    if currentjob == "police" or currentjob == "ambulance" or currentjob == "mechanic" or currentjob == "cardealer" then -- job's name here
         if not (currentjob == job.name) then
             TriggerServerEvent("utk_sl:jobchanged", currentjob, job.name, 1)
         end
     else
-        if job.name == "police" or job.name == "ambulance" then -- job's name here
+        if job.name == "police" or job.name == "ambulance" or job.name == "mechanic" or job.name == "cardealer" then -- job's name here
             TriggerServerEvent("utk_sl:jobchanged", currentjob, job.name, 0)
         end
     end
