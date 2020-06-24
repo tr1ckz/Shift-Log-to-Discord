9dc5e89d491db924db4cdeeff5110ea7b0fe5c6e
tr1ckz <48994713+tr1ckz@users.noreply.github.com>
2020-06-24 00:34:47 -0400

    changed to use RP name instead of steam/identifier

--- utk_shiftlog/server.lua
+++ utk_shiftlog/server.lua
@@ -7,28 +7,45 @@ local timers = { -- if you want more job shifts add table entry here same as the
     police = {
         {} -- don't edit inside
     },
-    -- fbi = {}
+    mechanic = {
+        {}
+
+    },
+    cardealer = {
+        {}
+        }
 }
+
 local dcname = "Shift Logger" -- bot's name
 local http = "" -- webhook for police
 local http2 = "" -- webhook for ems (you can add as many as you want)
+local http3 = "" -- mechanic
+local http4 = "" -- cardealer
 local avatar = "" -- bot's avatar
+local mechavatar = "https://www.dafont.com/forum/attach/orig/8/8/880398.jpg"
+local emsavatar = "https://discordapp.com/api/webhooks/725204635874361464/sJ0kNEkp64onedHHDriXa9jIxbWy1xzVPYvtFxVghFtgoQ-E_raioYnFoJzMwU1NaI-y"
+local policeavatar = "https://www.logolynx.com/images/logolynx/60/6081a70682994e87ee9dc3395928a5d8.jpeg"
+local cardealeravatar = "https://images.vexels.com/media/users/3/147726/isolated/preview/3c35c23c922833a71a94e7d5faf28b88-car-sale-service-logo-by-vexels.png"
 
 function DiscordLog(name, message, color, job)
     local connect = {
         {
             ["color"] = color,
-            ["title"] = "**".. name .."**",
+           --["title"] = "**".. name .."**",
             ["description"] = message,
             ["footer"] = {
-                ["text"] = "utkforeva",
+                ["text"] = "",
             },
         }
     }
     if job == "police" then
-        PerformHttpRequest(http, function(err, text, headers) end, 'POST', json.encode({username = dcname, embeds = connect, avatar_url = avatar}), { ['Content-Type'] = 'application/json' })
+        PerformHttpRequest(http, function(err, text, headers) end, 'POST', json.encode({username = dcname, embeds = connect, avatar_url = policeavatar}), { ['Content-Type'] = 'application/json' })
     elseif job == "ambulance" then
-        PerformHttpRequest(http2, function(err, text, headers) end, 'POST', json.encode({username = dcname, embeds = connect, avatar_url = avatar}), { ['Content-Type'] = 'application/json' })
+        PerformHttpRequest(http2, function(err, text, headers) end, 'POST', json.encode({username = dcname, embeds = connect, avatar_url = emsavatar}), { ['Content-Type'] = 'application/json' })
+    elseif job == "mechanic" then
+        PerformHttpRequest(http3, function(err, text, headers) end, 'POST', json.encode({username = dcname, embeds = connect, avatar_url = mechavatar}), { ['Content-Type'] = 'application/json' })
+    elseif job == "cardealer" then
+        PerformHttpRequest(http4, function(err, text, headers) end, 'POST', json.encode({username = dcname, embeds = connect, avatar_url = cardealeravatar}), { ['Content-Type'] = 'application/json' })
     end
 end
 
@@ -46,15 +63,33 @@ AddEventHandler("utk_sl:jobchanged", function(old, new, method)
     local header = nil
     local color = nil
 
+        local result = MySQL.Sync.fetchAll('SELECT firstname, lastname FROM users WHERE identifier = @identifier', {
+            ['@identifier'] = xPlayer.identifier
+        })
+
+        local firstname = result[1].firstname
+        local lastname  = result[1].lastname
+
+        local data = {
+
+            firstname = firstname,
+            lastname  = lastname,
+
+        }
+
+
     if old == "police" then
         header = "Police Shift" -- Header
         color = 3447003 -- Color
     elseif old == "ambulance" then
         header = "EMS Shift"
         color = 15158332
-    --elseif job == "fbi" then
-        --header = "FBI Shift"
-        --color = 3447003
+    elseif job == "mechanic" then
+        header = "Mechanic Shift"
+        color = 8421504
+    elseif job == "cardealer" then
+        header = "VCM Shift"
+        color = 8447718
     end
     if method == 1 then
         for i = 1, #timers[old], 1 do
@@ -70,7 +105,7 @@ AddEventHandler("utk_sl:jobchanged", function(old, new, method)
                 elseif duration >= 3600 then
                     timetext = tostring(math.floor(duration / 3600).." hours, "..tostring(math.floor(math.fmod(duration, 3600)) / 60)).." minutes"
                 end
-                DiscordLog(header , "Steam Name: **"..timers[old][i].name.."**\nIdentifier: **"..timers[old][i].identifier.."**\n Shift duration: **__"..timetext.."__**\n Start date: **"..date.."**\n End date: **"..os.date("%d/%m/%Y %X").."**", color, old)
+                DiscordLog(header , "Name: **"..timers[old][i].firstname.. " " ..timers[old][i].lastname.. "**\n Shift duration: **__"..timetext.."__**\n Start date: **"..date.."**\n End date: **"..os.date("%d/%m/%Y %X").."**", color, old)
                 table.remove(timers[old], i)
                 break
             end
@@ -83,9 +118,10 @@ AddEventHandler("utk_sl:jobchanged", function(old, new, method)
             end
         end
     end
-    if new == "police" or new == "ambulance" then
-        table.insert(timers[new], {id = xPlayer.source, identifier = xPlayer.identifier, name = xPlayer.name, time = os.time(), date = os.date("%d/%m/%Y %X")})
+    if new == "police" or new == "ambulance" or new == "mechanic" or new == "cardealer" then
+        table.insert(timers[new], {id = xPlayer.source, identifier = xPlayer.identifier, firstname = firstname, lastname = lastname, time = os.time(), date = os.date("%d/%m/%Y %X")})
     end
+
 end)
 
 AddEventHandler("playerDropped", function(reason)
@@ -106,6 +142,12 @@ AddEventHandler("playerDropped", function(reason)
                 elseif k == "ambulance" then
                     header = "EMS Shift"
                     color = 15158332
+                elseif job == "mechanic" then
+                    header = "Mechanic Shift"
+                    color = 8421504
+                elseif job == "cardealer" then
+                    header = "VCM Shift"
+                    color = 8447718
                 end
                 if duration > 0 and duration < 60 then
                     timetext = tostring(math.floor(duration)).." seconds"
@@ -114,7 +156,7 @@ AddEventHandler("playerDropped", function(reason)
                 elseif duration >= 3600 then
                     timetext = tostring(math.floor(duration / 3600).." hours, "..tostring(math.floor(math.fmod(duration, 3600)) / 60)).." minutes"
                 end
-                DiscordLog(header, "Steam Name: **"..timers[k][n].name.."**\nIdentifier: **"..timers[k][n].identifier.."**\n Shift duration: **__"..timetext.."__**\n Start date: **"..date.."**\n End date: **"..os.date("%d/%m/%Y %X").."**", color, k)
+                DiscordLog(header, "Steam Name: **"..timers[k][n].name.."**\n Shift duration: **__"..timetext.."__**\n Start date: **"..date.."**\n End date: **"..os.date("%d/%m/%Y %X").."**", color, k)
                 table.remove(timers[k], n)
                 return
             end
@@ -122,4 +164,4 @@ AddEventHandler("playerDropped", function(reason)
     end
 end)
 
-DiscordLog("[utk_shiftlog]", "Shift logger started!", 3447003, "police")
+--DiscordLog("[utk_shiftlog]", "Shift logger started!", 3447003, "police")
